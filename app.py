b0c1f6ec8aa699b0bceb63d5e9051b4e432f46ac
Elsa Villon <144967593+elsavillon@users.noreply.github.com>
2024-03-17 20:27:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,23 @@ def index():
     <p> Aqui começa um novo parágrafo.</p>
     </body>
 </html>"
+
+@app.route("/pix_no_brasil)
+def index():
+  return "<html>
+    <title>Aceita pix</title>
+    <body>
+        <h1> Aqui vai o título da home.</h1>
+    <p> Aqui começa um novo parágrafo.</p>
+    </body>
+</html>"
+
+@app.route("/pix_em_numeros)
+def index():
+  return "<html>
+    <title>Aceita pix</title>
+    <body>
+        <h1> Aqui vai o título da home.</h1>
+    <p> Aqui começa um novo parágrafo.</p>
+    </body>
+</html>"
