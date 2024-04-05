9772b7f088975b3bd8d93be189fc615ad9759f40
Your Name <giovannadcbs@gmail.com>
2024-04-05 14:54:40 -0300

    21

--- app.py
+++ app.py
@@ -26,10 +26,34 @@ def curriculo ():
 def curriculo ():
     return render_template("bbc.html")
 
-@app.route('/reportagens.html', methods=['GET'])
+@app.route("/reportagens.html", methods=["GET"])
 def result():
-    tema = request.args.get('tema')
+    tema = request.args.get("tema")
     materias = exibir_top5(tema)
-    return render_template('reportagens.html', tema=tema, materias=materias)
+    return render_template("reportagens.html", tema=tema, materias=materias)
 
+def exibir_top5(tema):
+  url = 'https://www.bbc.com/portuguese/topics/' + tema
 
+  response = requests.get(url)
+  soup = BeautifulSoup(response.text, 'html.parser')
+
+  h2 = soup.find_all('h2') # look for all titles 
+  titulos = []
+  for h in h2:
+    titulo = h.get_text()
+    titulos.append(titulo)
+
+  titulos = titulos[:5] # select only first 5
+  
+  links = soup.find_all('a', {'class' : 'focusIndicatorDisplayBlock bbc-uk8dsi e1d658bg0'})
+  
+  hrefs = []
+  for href in links:
+    hrefs.append(href.get('href'))
+
+  hrefs = hrefs[:5]
+
+  materias = list(zip(titulos, hrefs))
+
+  return materias
