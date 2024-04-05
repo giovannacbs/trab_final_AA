2734c15ece32d8d651812a8edad554dbe1c2d6d0
Your Name <giovannadcbs@gmail.com>
2024-04-05 14:40:53 -0300

    17

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+## Primeira parte do trabalho
+
 @app.route("/")
 def index():
     return render_template("home.html")
@@ -14,6 +16,16 @@ def portifolio ():
 def curriculo ():
     return render_template("curriculo.html")
 
+## Parte da página dinâmica com raspagem do BBC PT
 
+@app.route("/bbc")
+def curriculo ():
+    return render_template("bbc.html")
+
+@app.route('/reportagens.html', methods=['GET'])
+def result():
+    tema = request.args.get('tema')
+    materias = exibir_top5(tema)
+    return render_template('reportagens.html', tema=tema, materias=materias)
 
 
